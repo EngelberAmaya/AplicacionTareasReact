34a5621bfb2059d6b0f8dea2d4b14860ede71a4c
EngelberAmaya <engelberamaya@gmail.com>
2023-02-24 15:35:53 -0400

    useEffect - Warning

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Message = () => {
 
+    const [coords, setCoords] = useState({x: 0, y:0});
+    const {x, y} = coords;
+
     useEffect(() => {
-        console.log('componente montado')
+
+        const mouseMove = (e) => {
+            const coords = { x: e.x, y: e.y}
+            setCoords(coords);
+        }
+
+        window.addEventListener('mousemove', mouseMove)
 
         return () => {
-            console.log('componente desmontado')
+            //console.log('componente desmontado')
+            window.removeEventListener('mousemove', mouseMove)
         }
 
     }, []);
@@ -15,6 +24,9 @@ const Message = () => {
     return (
         <div>
             <h3>Eres genial</h3>
+            <p>
+                x: {x} y: {y}
+            </p>
         </div>
     )
 }
