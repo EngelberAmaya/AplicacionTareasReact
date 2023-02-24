594e86291f1a8b961717826e7c1173a28b70058f
EngelberAmaya <engelberamaya@gmail.com>
2023-02-24 11:43:16 -0400

    useCounter - CustomHook

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CounterApp from './components/01-useState/CounterApp';
+//import CounterApp from './components/01-useState/CounterApp';
+import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 //import HookApp from './HookApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </React.StrictMode>
 );
 
