1f1e6c63df71c80014c8248ebafe887c28b026a8
EngelberAmaya <engelberamaya@gmail.com>
2023-03-09 18:44:15 -0400

    General idea of ​​a reducer - Via code

--- src/index.js
+++ src/index.js
@@ -12,19 +12,21 @@ import ReactDOM from 'react-dom/client';
 // import Layout from './components/05-useLayoutEffect/Layout';
 // import Memorize from './components/06-memos/Memorize';
 // import MemoHook from './components/06-memos/MemoHook';
-import Padre from './components/07-prueba-memo/Padre';
+//import Padre from './components/07-prueba-memo/Padre';
 
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/* <CounterWithCustomHook /> */}
-    <Padre />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     {/* <CounterWithCustomHook /> */}
+//     <Padre />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
+
+import './components/08-useReducer/intro-reducer'
