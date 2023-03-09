ed03bc7e26d7e7c35709c7eb84d429957d5afaab
EngelberAmaya <engelberamaya@gmail.com>
2023-03-09 19:46:03 -0400

    Creating the shell of the TODOs list

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -15,15 +15,46 @@ const TodoApp = () => {
     console.log(todos);
 
     return (
-        <div>
-            <h1>TodoApp</h1>
+        <div className='container'>
+            <h1>TodoApp ({todos.length})</h1>
             <hr/>
 
-            <ul>
-                <li>Hola</li>
-                <li>Hola Mundo</li>
-                <li>Hola de nuevo</li>
-            </ul>
+            <div className='row'>
+
+                <div className='col-7'>
+
+                    <ul className='list-group list-group-flush'>
+
+                        {
+                            todos.map((todo, i) => (
+                                <li 
+                                    key={todo.id} 
+                                    className="list-group-item">
+                                        <p className='text-center'>{i+1}. {todo.desc}</p>
+                                        <button className='btn btn-danger'>Borrar</button>
+                                </li>
+                            ))
+                        }
+
+                    </ul>
+
+                </div>
+
+                <div className='col-5'>
+
+                    <h4>Agregar TODO</h4>
+                    <hr/>
+
+                    <form>
+                        <input type="text" name='description' placeholder='Aprender...' autoComplete='false' className='form-control' />
+
+                        <button className='btn btn-outline-primary mt-2 btn-block'>Agregar</button>
+                    </form>
+                   
+                </div>
+
+            </div>
+
         </div>
     )
 }
