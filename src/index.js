c8af17bdd1d499dcff14948bf769e2ad8bc7af64
EngelberAmaya <engelberamaya@gmail.com>
2023-03-10 13:20:55 -0400

    Updating the docs folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 //import HookApp from './HookApp';
 // import CounterApp from './components/01-useState/CounterApp';
